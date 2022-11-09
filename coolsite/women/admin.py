32416ea7a5eae12b4c32a1134853718e8e421278
AnatoliyKyznichenko <antonkorodan@gmail.com>
2022-11-09 23:07:50 +0200

    cc

--- coolsite/women/admin.py
+++ coolsite/women/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import *
 
-
+#
 class WomenAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
     list_display_links = ('id', 'title')
